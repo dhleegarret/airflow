28629002684ce3d68fdb152b3464540460574f46
dhleegarret <dhleegarret@gmail.com>
2026-08-14 11:20:20 +0900

    커넥션 테스트용 dag 업로드(수정)

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -1,37 +1,46 @@
+from airflow.sdk import DAG, chain
+import datetime
+import pendulum
 from airflow.providers.standard.operators.empty import EmptyOperator #아무것도 하지 않는 오퍼레이터(=설계도)
 
-t1 = EmptyOperator(
-    task_id = "t1"
-)
+with DAG(
+    dag_id="dags_conn_test",
+    schedule=None,
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul")
+    catchup=False
+) as dag:
+    t1 = EmptyOperator(
+        task_id = "t1"
+    )
 
-t2 = EmptyOperator(
-    task_id = "t2"
-)
+    t2 = EmptyOperator(
+        task_id = "t2"
+    )
 
-t3 = EmptyOperator(
-    task_id = "t3"
-)
+    t3 = EmptyOperator(
+        task_id = "t3"
+    )
 
-t4 = EmptyOperator(
-    task_id = "t4"
-)
+    t4 = EmptyOperator(
+        task_id = "t4"
+    )
 
-t5 = EmptyOperator(
-    task_id = "t5"
-)
+    t5 = EmptyOperator(
+        task_id = "t5"
+    )
 
-t6 = EmptyOperator(
-    task_id = "t6"
-)
+    t6 = EmptyOperator(
+        task_id = "t6"
+    )
 
-t7 = EmptyOperator(
-    task_id = "t7"
-)
+    t7 = EmptyOperator(
+        task_id = "t7"
+    )
 
-t8 = EmptyOperator(
-    task_id = "t8"
-)
+    t8 = EmptyOperator(
+        task_id = "t8"
+    )
 
-t1 >> [t2, t3] >> t4
-t5 >> t4
-[t4, t7] >> t6 >> t8
+    t1 >> [t2, t3] >> t4
+    t5 >> t4
+    [t4, t7] >> t6 >> t8
