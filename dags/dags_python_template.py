b99c4404b17fc5459d0cd99454e7b746fa01f144
dhleegarret <dhleegarret@gmail.com>
2026-08-21 13:52:21 +0900

    python template 수정

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -10,7 +10,7 @@ with DAG(
     catchup=False,  
 ) as dag:
 
-    # 첫 번째 방법
+    # 첫 번째 방법: op_kwargs
     def python_function1(start_date, end_date, **kwargs):
         print(start_date)
         print(end_date)
@@ -21,7 +21,7 @@ with DAG(
         op_kwargs={'start_date':'{{data_interval_start | ds}}', 'end_date': '{{data_interval_end | ds}}'}
     )
 
-    # 두 번째 방법
+    # 두 번째 방법: 이미 정의되어있는 kwargs
     @task(task_id='python_t2')
     def python_function2(**kwargs):
         print(kwargs)
@@ -29,6 +29,6 @@ with DAG(
         print('ts:' + kwargs['ts'])
         print('data_interval_start:' + str(kwargs['data_interval_start']))
         print('data_interval_end:' + str(kwargs['data_interval_end']))
-        print('task_instance:' + str(kwargs['t1']))
+        print('task_instance:' + str(kwargs['t2']))
 
     python_t1 >>  python_function2()    #python_function2(): 데코레이터 함수명으로만 지정해도 실행 가능한 Task로 생성됨
