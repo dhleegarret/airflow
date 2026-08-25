5a6bb689ea1e31d4030aab6f8e3af18555e93089
dhleegarret <dhleegarret@gmail.com>
2026-08-25 16:06:57 +0900

    python 오퍼레이터 with 매크로

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -10,7 +10,7 @@ with DAG(
     catchup=False,  
 ) as dag:
 
-    # task 1: macros를 활용한 날짜 연산
+    # task 1: macros를 활용한 날짜 연산 (start_date: 전월 1일, end_date: 전월 마지막일)
     @task(task_id='task_using_macros',
           templates_dict={'start_date': '{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
                         'end_date': '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
@@ -24,7 +24,7 @@ with DAG(
             print(start_date)
             print(end_date)
 
-    # task 2: 날짜 직접 연산
+    # task 2: 날짜 직접 연산 (start_date: 전월 1일, end_date: 전월 마지막일)
     @task(task_id='task_direct_calc')
     def get_datetime_calc(**kwargs):
         from dateutil.relativedelta import relativedelta    # 라이브러리를 함수안에 작성한 이유: DAG 스케쥴러 부하를 경감하기 위함 (DAG이 주기적으로 문법 오류 존재 여부를 파싱하는데, 데코레이터, 오퍼레이터 함수안에 선언한 라이브러리는 검사하지 않음)
