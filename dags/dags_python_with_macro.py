a1b71c6aab019793d2436801dec4265c61115001
dhleegarret <dhleegarret@gmail.com>
2026-08-25 16:00:21 +0900

    python 오퍼레이터 with 매크로

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -15,11 +15,6 @@ with DAG(
           templates_dict={'start_date': '{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
                         'end_date': '{{ (data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
           }
-
-
-        
-
-
     )
     def get_datetime_macro(**kwargs):
         templates_dict = kwargs.get('templates_dict') or {}
