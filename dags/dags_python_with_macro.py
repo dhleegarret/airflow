d9eaba1652c40d8f498be373d5d92402672661de
dhleegarret <dhleegarret@gmail.com>
2026-08-25 15:50:26 +0900

    python 오퍼레이터 with 매크로

---
+++ dags/dags_python_with_macro.py
@@ -0,0 +1,38 @@
+from airflow.sdk import DAG, task
+import datetime
+import pendulum
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_python_with_macro",
+    schedule="10 0 * * *",              
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"), 
+    catchup=False,  
+) as dag:
+
+    # task 1: macros를 활용한 날짜 연산
+    @task(task_id='task_using_macros',
+          templates_dict={'start_date': '{{ (data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds }}',
+                        'end_date': '{{ (data_interval_end.in_timezone("Asia/Seoul") + replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
+          }
+    )
+    def get_datetime_macro(**kwargs):
+        templates_dict = kwargs.get('templates_dict') or {}
+        if templates_dict:
+            start_date = templates_dict.get('start_date') or 'start_date없음'
+            end_date = templates_dict.get('end_date') or 'end_date없음'
+            print(start_date)
+            print(end_date)
+
+    # task 2: 날짜 직접 연산
+    @task(task_id='task_direct_calc')
+    def get_datetime_calc(**kwargs):
+        from dateutil.relativedelta import relativedelta    # 라이브러리를 함수안에 작성한 이유: DAG 스케쥴러 부하를 경감하기 위함 (DAG이 주기적으로 문법 오류 존재 여부를 파싱하는데, 데코레이터, 오퍼레이터 함수안에 선언한 라이브러리는 검사하지 않음)
+
+        data_interval_end = kwargs['data_interval_end']
+        prev_month_day_first = data_interval_end.in_timezone("Asia/Seoul") + relativedelta(months=-1, day=1)
+        prev_month_day_last = data_interval_end.in_timezone("Asia/Seoul") + replace(day=1) + relativedelta(months=-1)
+        print(prev_month_day_first.strftime('%Y-%m-%d'))
+        print(prev_month_day_last.strftime('%Y-%m-%d'))
+
+    get_datetime_macro() >> get_datetime_calc()
